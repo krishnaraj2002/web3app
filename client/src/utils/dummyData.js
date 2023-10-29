a2f5541cd684c1441e5b0d68d5331ce28a8d648e
KRISHNA RAJ A <krishnarajanbazhagan@gmail.com>
2023-10-29 15:51:54 +0530

    Initial commit

---
+++ client/src/utils/dummyData.js
@@ -0,0 +1,13 @@
+
+
+export default [
+  {
+    id: 1,
+    url: "https://media.tenor.com/OigfSw7_HmMAAAAC/explosion-boom.gif",
+    message: "Boom",
+    timestamp: "07/29/2023",
+    addressFrom: "0xCF8e569A97C423952DdFf902375C7C76549A6A90",
+    amount: "0.01",
+    addressTo: "0x8aa395Ab97837576aF9cd6946C79024ef1acfdbE",
+  },
+];
